9d7e1fa44c7dec2faebec7c5869af9cab476e763
Hugo Hu <hugo@hugohu.me>
2026-01-11 11:02:35 -0500

    modify navbar

--- site/app/components/nav.tsx
+++ site/app/components/nav.tsx
@@ -21,13 +21,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="mb-16 tracking-tight md:-ml-[8px]">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start justify-end w-full relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start justify-end w-full relative px-0 pb-0 fade overflow-x-auto whitespace-nowrap scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-0 pr-0">
             {Object.entries(navItems).map(([path, { name, newTab }]) => {
               return (
                 <Link
