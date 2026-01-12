772c80c71b27f9940f158171102add9808e9db48
Hugo Hu <hugo@hugohu.me>
2026-01-11 20:47:04 -0500

    update upload page

--- site/app/admin/page.tsx
+++ site/app/admin/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 export default function AdminPage() {
   return (
     <section>
       <h1 className="title font-semibold text-2xl tracking-tighter">Admin</h1>
       <p className="mt-2 text-sm text-neutral-600 dark:text-neutral-400">
-        You are authenticated. Add admin-only tools here.
+        Authenticated.
       </p>
+
+      <Link href="/admin/upload">Image Upload Page</Link>
     </section>
   );
 }
