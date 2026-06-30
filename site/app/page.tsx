333b3d4beeddf3a6121e0b3da0ed54f1f07dce80
Hugo Hu <hugo@hugohu.me>
2026-06-30 02:44:54 -0400

    push

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -7,10 +7,18 @@ export default function Page() {
         <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
           Hugo Hu
         </h1>
+        {/* <p className="mb-4">
+          {`Hi! I'm a senior at Stuyvesant High School in New York City, and I'm an incoming 
+          first-year to the State University of New York at Stony Brook, where I will pursue
+          a double major in Computer Science (Honors) and Applied Math & Statistics, with a minor in 
+          Political Science. I'll also be a member of the Honors College.
+          In my free time, I love photography, cooking, and PCB design. `}
+        </p> */}
         <p className="mb-4">
-          {`Hi! I'm a senior at Stuyvesant High School in New York City. I'm passionate about
-        photography and cooking, and I shoot for my school's newspaper and yearbook. I'm also 
-        a part of Stuyvesant's math team. `}
+          {`Hi! I'm a freshman at the State University of New York at Stony Brook, where I will pursue
+          a double major in Computer Science (Honors) and Applied Math & Statistics, with a minor in 
+          Political Science. I am also a member of the Honors College. In my free time, I love photography,
+          cooking, and designing PCBs.`}
         </p>
         <p className="mb-4">
           {`Previously, I've conducted research at MSKCC on machine learning
